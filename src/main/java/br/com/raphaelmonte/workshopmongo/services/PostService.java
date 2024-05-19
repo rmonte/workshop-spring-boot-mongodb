2eecf53af600fbb54a34f9dabb6e7358630e55de
Raphael Monte <raphael.monte@icloud.com>
2024-05-19 18:29:05 -0300

    Obtendo o post por id

---
+++ src/main/java/br/com/raphaelmonte/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package br.com.raphaelmonte.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.raphaelmonte.workshopmongo.domain.Post;
+import br.com.raphaelmonte.workshopmongo.repository.PostRepository;
+import br.com.raphaelmonte.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+}
