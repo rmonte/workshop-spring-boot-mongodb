f3c2a6d5b76baa9a6367351aa6afce56f333ea93
Raphael Monte <raphael.monte@icloud.com>
2024-05-19 17:06:10 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/br/com/raphaelmonte/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package br.com.raphaelmonte.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.raphaelmonte.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
