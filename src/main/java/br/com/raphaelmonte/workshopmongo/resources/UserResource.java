52afd6cff1264818af204a772b43e23dfed76832
Raphael Monte <raphael.monte@icloud.com>
2024-05-19 18:09:25 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/br/com/raphaelmonte/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/raphaelmonte/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.raphaelmonte.workshopmongo.domain.Post;
 import br.com.raphaelmonte.workshopmongo.domain.User;
 import br.com.raphaelmonte.workshopmongo.dto.UserDTO;
 import br.com.raphaelmonte.workshopmongo.services.UserService;
@@ -62,6 +63,13 @@ public class UserResource {
 		service.delete(id);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
