d5399e05f7e77eccc35f2017da20e3295fbd058c
Raphael Monte <raphael.monte@icloud.com>
2024-05-19 20:32:25 -0300

    Consulta simples com query method

--- src/main/java/br/com/raphaelmonte/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/raphaelmonte/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package br.com.raphaelmonte.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.raphaelmonte.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
